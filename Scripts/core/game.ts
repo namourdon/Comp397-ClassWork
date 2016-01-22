27377d15d51433895931e91b0725a7c45bec4c21
namourdon <nash.amourdon@gmail.com>
2016-01-22 18:24:10 -0500

    l

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -17,6 +17,7 @@ function init():void{
 
 function gameLoop(event:createjs.Event):void{
     stage.update();
+    helloLabel.rotation+=5;
     
 }
 function main(){
@@ -24,6 +25,8 @@ function main(){
     helloLabel= new createjs.Text("Hello world","60px Consolas","#000000");
     helloLabel.regX= helloLabel.getMeasuredWidth()*0.5;
     helloLabel.regY=helloLabel.getMeasuredHeight()*0.5;
+    helloLabel.x=320;
+    helloLabel.y=240;
     stage.addChild(helloLabel);
 }
 
